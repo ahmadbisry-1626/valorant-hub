f0a2ab6387865ec84b788603334cea9b6bb9cc7c
ahmadbisry-1626 <smakarya057@gmail.com>
2025-10-02 15:51:39 +0700

    chore: update dependencies and devDependencies in package.json
    - Added new dependencies: @radix-ui/react-slot, @tanstack/react-query, axios, class-variance-authority, clsx, framer-motion, lucide-react, react-icons, tailwind-merge, zustand.
    - Updated devDependencies: @eslint/eslintrc, @tailwindcss/postcss, tailwindcss, typescript.
    - Removed unused dependencies and devDependencies.
    
    refactor: remove unused SVG files
    
    - Deleted obsolete SVG files: file.svg, globe.svg, next.svg, vercel.svg, window.svg.
    
    feat: add new image assets
    
    - Added new image files: abstract-1.png, abstract-2.png, abstract-main.png, glass.png, valorant-logo.png.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen w-full items-center justify-center flex-col ">
+            {/* md:max-w-7xl mx-auto px-5 md:px-6 */}
+            <HeroSection />
+        </div>
+    );
+}
---
+++ components/HeroSection.tsx
@@ -0,0 +1,73 @@
+import Image from 'next/image'
+import React from 'react'
+
+const AbstractImage = ({ path, width, height, className }: { path: string, width: number, height: number, className: string }) => {
+    return (
+        <Image
+            src={path}
+            alt='abstract object'
+            width={width}
+            height={height}
+            sizes='100vw'
+            className={className}
+        />
+    )
+}
+
+const HeroSection = () => {
+    return (
+        <div className='w-full min-h-screen flex items-center justify-center relative'>
+            <div className='absolute md:bottom-32 bottom-20 md:left-80 left-32 flex items-center gap-1'>
+                <div className='md:size-3 size-2 bg-main' />
+                <div className='md:size-3 size-2 bg-main' />
+
+            </div>
+            <div className='absolute md:bottom-20 bottom-10 md:left-72 left-20 size-5 bg-main' />
+            <AbstractImage
+                path='/images/abstract-1.png'
+                width={70}
+                height={70}
+                className='absolute bottom-20 md:bottom-40 md:left-20 left-10'
+            />
+            <div className='absolute lg:top-24 top-10 lg:left-60 md:left-32 left-10'>
+                <div className='size-4 md:size-5 bg-main lg:-translate-x-40' />
+                <AbstractImage
+                    path='/images/glass.png'
+                    width={200}
+                    height={200}
+                    className='md:w-[200px] md:h-[180px] w-[150px] h-[130px]'
+                />
+            </div>
+            <AbstractImage
+                path='/images/abstract-2.png'
+                width={80}
+                height={80}
+                className='absolute lg:top-20 top-10 lg:right-52 md:right-32 right-10'
+            />
+            <div className='absolute bottom-0 right-0'>
+                <div className='flex items-center gap-1 md:translate-x-32 translate-x-24 md:translate-y-6 w-max max-md:hidden'>
+                    <div className='size-2 md:size-3 bg-black' />
+                    <div className='size-2 md:size-3 bg-black' />
+                </div>
+                <AbstractImage
+                    path='/images/abstract-main.png'
+                    width={300}
+                    height={300}
+                    className='md:w-[300px] md:h-[415px] w-[200px] h-[315px]'
+                />
+            </div>
+
+            <div className='flex flex-col gap-5 md:gap-10 items-center z-10 relative'>
+                <Image src={'/images/valorant-logo.png'} alt='logo valorant' width={250} height={250} sizes='100vw' className='md:w-[250px] md:h-[205px] w-[200px] h-[160px]' />
+                <div className='flex flex-col gap-3 items-center'>
+                    <h1 className='md:text-5xl text-4xl text-main'>Valorant Hub</h1>
+                    <p className='md:text-2xl text-lg text-center md:max-w-xl max-w-md'>
+                        Your go-to source for strategies, updates, and competitive insights
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
